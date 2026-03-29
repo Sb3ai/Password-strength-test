af4e40d32ae79e2bb78e83aabc2cc5f4d4624bfa
Sb3ai <101978488+Sb3ai@users.noreply.github.com>
2026-03-29 15:05:07 +0300

    Update v2
    A new feature has been added.
    
    [+] Feature:
    [1] - Firstly added a list
    [2]] - Secondly a for loop to see the password characters
    [3] - Detect wether the symbol is in the password. if so strength point is increased
    
    Future adds:
    [1] - detect to see there are ints inside the password , i.e (numbers in password)

--- Main.py
+++ Main.py
@@ -7,12 +7,20 @@ Created on Sun Mar 29 14:41:55 2026
 
 def user_input():
     point = 0
+    symbols = ["!","@","#","$","%","^","&","*","()","_","-","+","~","`","'",".","/",";","(","[","]","/",">","<"]
     Password = input("Please enter a password >> ")
     
     # We check the length of the password here.
     if len(Password) >= 8 and len(Password) <= 64:
         point += 1
     
+    
+    # Here we implemented a system that see's the list of symbols to detect wether if their are symbols in the password
+    # Hence increasing the strength point
+    for sym in symbols:
+        if sym in Password:
+            point += 1
+            break
     return point
 
 
