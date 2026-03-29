60000820bdac81fe8dba167543ba84d97097aa06
Sb3ai <101978488+Sb3ai@users.noreply.github.com>
2026-03-30 02:41:11 +0300

    Update v3
    [+] Features
    [1] - Added a working if statement that detects integers in the password
    [2] - Added a if statement to detect capitalize letters in the password
    [3] - Added a bonus point if the password is bigger than 12 or equal to it whilst being less than 64
    
    ^^^ = A good password would equal to 5/5 points.

--- Main.py
+++ Main.py
@@ -14,7 +14,18 @@ def user_input():
     if len(Password) >= 8 and len(Password) <= 64:
         point += 1
     
+    if len(Password) >= 12 and len(Password) <= 64: # This is a bonus point, if the password is longer than or equal to 12 then extra points
+        point += 1
     
+    for letter in Password:
+        if letter.isupper(): # Here we detect if the password contains Capitalized words
+            print(letter)
+            point += 1
+            break
+    for i in Password:
+        if i.isdigit(): # Here we detect if a the password contains any integar inside the password. if so point ++ 1
+            point += 1
+            break
     # Here we implemented a system that see's the list of symbols to detect wether if their are symbols in the password
     # Hence increasing the strength point
     for sym in symbols:
