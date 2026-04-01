2f0205e2605968a4876cf41c083a48221df76696
Sb3ai <saad.almansoori07@gmail.com>
2026-04-01 12:39:01 +0100

    Update v2
    Transformed the code to a class.

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ Created on Sun Mar 29 14:41:55 2026
 
 @author: bu3li
 """
-
+"""
 def user_input():
     point = 0
     symbols = ["!","@","#","$","%","^","&","*","()","_","-","+","~","`","'",".","/",";","(","[","]","/",">","<"]
@@ -37,3 +37,52 @@ def user_input():
 
 fetch_points = user_input()
 print(fetch_points)
+"""
+
+class Password_Checker:
+    def __init__(obj, password, point=0):
+        obj.password = password
+        obj.symbols = ["!","@","#","$","%","^","&","*","()","_","-","+","~","`","'",".","/",";","(","[","]","/",">","<"]
+        obj.point = point
+    
+    def checkLen(obj):
+        if len(obj.password) >= 8 and len(obj.password) <= 64:
+            obj.point += 1
+        if len(obj.password) >= 12 and len(obj.password) <= 64:
+            obj.point += 1
+    
+    def checkLetters(obj):
+        for letter in obj.password:
+            if letter.isupper():
+                obj.point += 1
+                break
+    def checkNums(obj):
+        for number in obj.password:
+            if number.isdigit():
+                obj.point += 1
+                break
+                
+    def checkSym(obj):
+        for letter in obj.password:
+            if letter in obj.symbols:
+                obj.point += 1
+                break
+    def __str__(obj):
+        obj.checkLen()
+        obj.checkLetters()
+        obj.checkNums()
+        obj.checkSym()
+        print(f"The password {obj.password} strength is [{obj.point}/5]")
+        if obj.point <= 2:
+            print("Your password is considered weak.")
+        elif obj.point <= 3:
+            print("Your password is conisdered good")
+        elif obj.point <= 4:
+            print("Your password is strong and good.")
+    
+
+user_input = input("Enter your password >> ")
+
+user = Password_Checker(user_input)
+user.__str__()
+            
